0c4315ee1e42537e72549d57d011fa150ae15d98
raph559 <raph.richaud@gmail.com>
2026-03-06 00:20:03 +0100

    clean: remove temporary test and patch scripts

--- test_marked.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var marked_highlight_1 = require("marked-highlight");
-var ext = (0, marked_highlight_1.markedHighlight)({ highlight: function (c) { return c; } });
-console.log(ext.renderer.code);
--- test_marked.ts
+++
@@ -1,1 +0,0 @@
-import { markedHighlight } from 'marked-highlight'; const ext: any = markedHighlight({ highlight: c => c }); console.log(ext.renderer.code);
